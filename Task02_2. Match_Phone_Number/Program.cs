5ca2fe35a586e0a72870f7783a58a66f39f15774
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-21 21:08:02 +0200

    Add files via upload

---
+++ Task02_2. Match_Phone_Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task02_Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"\+359(-| )2\1\d{3}\1\d{4}\b";
+
+            string sofiaPhoneNumbers = Console.ReadLine();
+
+            MatchCollection validSofiaPhoneNumbers = Regex.Matches(sofiaPhoneNumbers, patern);
+
+            
+                Console.WriteLine(string.Join(", ", validSofiaPhoneNumbers));
+        }
+    }
+}
