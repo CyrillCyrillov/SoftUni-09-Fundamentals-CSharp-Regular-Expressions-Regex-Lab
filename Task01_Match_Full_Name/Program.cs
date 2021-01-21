161ed9338cf31cd558cf877e31931c00cf1ee4a1
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-21 21:07:02 +0200

    Add files via upload

---
+++ Task01_Match_Full_Name/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _Task01_Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            string names = Console.ReadLine();
+
+            MatchCollection validNames = Regex.Matches(names, patern);
+
+            foreach (Match name in validNames)
+            {
+                Console.Write($"{name.Value} ");
+            }
+            
+        }
+    }
+}
